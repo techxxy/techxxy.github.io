fa4cd3239ae97ef198a3cf509f7e03383a2bd984
techxxxy <jeeyoung.park0@gmail.com>
2024-02-16 17:54:06 +0100

    update as views=0

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -2,8 +2,7 @@
 
 import projects__appleKeyboardMdx from './projects__appleKeyboard.mdx.json' assert { type: 'json' }
 import projects__boilerplatedeploymentMdx from './projects__boilerplatedeployment.mdx.json' assert { type: 'json' }
-import projects__csshtmlfrenzyMdx from './projects__csshtmlfrenzy.mdx.json' assert { type: 'json' }
 import projects__fireupkoreanalphabetMdx from './projects__fireupkoreanalphabet.mdx.json' assert { type: 'json' }
 import projects__techxxyGithubIoMdx from './projects__techxxy.github.io.mdx.json' assert { type: 'json' }
 
-export const allProjects = [projects__appleKeyboardMdx, projects__boilerplatedeploymentMdx, projects__csshtmlfrenzyMdx, projects__fireupkoreanalphabetMdx, projects__techxxyGithubIoMdx]
+export const allProjects = [projects__appleKeyboardMdx, projects__boilerplatedeploymentMdx, projects__fireupkoreanalphabetMdx, projects__techxxyGithubIoMdx]
