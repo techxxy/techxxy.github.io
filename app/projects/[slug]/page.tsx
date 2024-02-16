8ca3ac8afdc55daac6395bf8df0d6078cdb0588e
techxxxy <jeeyoung.park0@gmail.com>
2024-02-16 17:37:44 +0100

    update as views=0

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -33,7 +33,7 @@ export default async function PostPage({ params }: Props) {
 
   return (
     <div className="bg-zinc-50 min-h-screen">
-      <Header project={project} views={views} />
+      <Header project={project} views={0} />
 
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
         <Mdx code={project.body.code} />
