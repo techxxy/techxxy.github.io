38d3ba793e10f3c49c3e8c5057d76bc02c5f2ae8
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-01 23:11:41 +0100

    add a new post

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -4,6 +4,7 @@ import projects__appleKeyboardMdx from './projects__appleKeyboard.mdx.json' asse
 import projects__boilerplatedeploymentMdx from './projects__boilerplatedeployment.mdx.json' assert { type: 'json' }
 import projects__documentationMdx from './projects__documentation.mdx.json' assert { type: 'json' }
 import projects__fireupkoreanalphabetMdx from './projects__fireupkoreanalphabet.mdx.json' assert { type: 'json' }
+import projects__iconsMdx from './projects__icons.mdx.json' assert { type: 'json' }
 import projects__techxxyGithubIoMdx from './projects__techxxy.github.io.mdx.json' assert { type: 'json' }
 
-export const allProjects = [projects__appleKeyboardMdx, projects__boilerplatedeploymentMdx, projects__documentationMdx, projects__fireupkoreanalphabetMdx, projects__techxxyGithubIoMdx]
+export const allProjects = [projects__appleKeyboardMdx, projects__boilerplatedeploymentMdx, projects__documentationMdx, projects__fireupkoreanalphabetMdx, projects__iconsMdx, projects__techxxyGithubIoMdx]
